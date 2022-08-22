44acdeb18095cf8b076ac62bd30424606e0c004d
uvucunicorn <103804307+uvucunicorn@users.noreply.github.com>
2022-08-23 01:41:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-def get_time():
-  return today['today']
-
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
